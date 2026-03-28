7b1bc3bc2b8d8cb3113d02e7216f1cd045f50398
Vyacheslav <slavaslava.s@yandex.ru>
2026-03-28 22:17:20 +0300

    Урок 6, обычное задание - ГОТОВО

--- script.js
+++ script.js
@@ -1,18 +1,83 @@
 `use strict`;
 
-//
+// Создаем общую функцию для запуска игры "Отгадай число"
 const guessTheNumber = function () {
-    let guessNum = 0;
+    // ------------
+    // Создаем замыкание для получения рандомного числа, которое потом будем отгадывать
 
     let getGuessNumber = function () {
-        let userInput = function () {
-            return (guessNum = +prompt("Введите число для отгадывания", 50));
+        let randomNumber = 0;
+        randomNumber = Math.floor(Math.random() * 100) + 1;
+        return function () {
+            return randomNumber;
         };
-        return userInput();
     };
 
-    let numberWhatIGuess = getGuessNumber();
-    console.log(numberWhatIGuess);
+    // Функция проверки на строковое значение
+    const isString = function (str) {
+        for (let i = 0; i < 10; i++) {
+            if (str.includes(String(i)) || str.trim().length === 0) {
+                return false;
+            }
+        }
+        return true;
+    };
 
-    let userInput = +prompt("Угадай число от 1 до 100", "Введите число");
+    const isNumber = function (num) {
+        if (isNaN(num)) {
+            return true;
+        } else {
+            return false;
+        }
+    };
+
+    let randomGuessNumber = getGuessNumber();
+    console.log(randomGuessNumber());
+
+    // Создаем рекурсию и условия
+    const userPlay = function () {
+        let userInput = prompt("Угадай число от 1 до 100", 50);
+
+        if (!userInput) {
+            return alert("Игра окончена");
+        }
+
+        if (isNumber(userInput)) {
+            alert("Введи число!");
+            let answer = confirm("Введите новый вариант");
+            if (!answer) {
+                return alert("Игра окончена");
+            } else {
+                userPlay();
+            }
+        }
+
+        if (Number(userInput) === randomGuessNumber()) {
+            return alert("Поздравляю, Вы угадали!!!");
+        }
+
+        if (Number(userInput) > randomGuessNumber()) {
+            alert("Загаданное число меньше");
+            let answer = confirm("Введите новый вариант");
+            if (!answer) {
+                return alert("Игра окончена");
+            } else {
+                userPlay();
+            }
+        }
+
+        if (Number(userInput) < randomGuessNumber()) {
+            alert("Загаданное число больше");
+            let answer = confirm("Введите новый вариант");
+            if (!answer) {
+                return alert("Игра окончена");
+            } else {
+                userPlay();
+            }
+        }
+    };
+    return userPlay();
 };
+
+// Запуск глобальной функции для отладки
+guessTheNumber();
